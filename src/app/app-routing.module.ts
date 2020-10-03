39ce79cc6ec73da8379804b8e8554e2974a90de6
Wojciech W <wojtekkk00@o2.pl>
2020-10-03 21:33:43 +0200

    base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { AlbumsListComponent } from './components/albums-list/albums-list.component';
+import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "about",
+    component: AboutComponent
+  },
+  {
+    path: "albums",
+    component: AlbumsListComponent
+  },
+  {
+    path: "**",
+    component: PageNotFoundComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { AlbumItemComponent } from './components/album-item/album-item.component';
+import { AlbumsListComponent } from './components/albums-list/albums-list.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    AlbumItemComponent,
+    AlbumsListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
